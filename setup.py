5c509ea348f3109231e818e67eac9f33333dac69
Suriyan Laohaprapanon <suriyant@gmail.com>
2021-07-15 11:39:45 +0000

    pinned scikit-learn to 0.22.2.post1

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.0',
+    version='0.2.1',
 
     description='Predict Soft News',
     long_description=long_description,
@@ -119,7 +119,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         "pandas",
-        "scikit-learn",
+        "scikit-learn==0.22.2.post1",
         "joblib",
         "tqdm",
         "nltk"
