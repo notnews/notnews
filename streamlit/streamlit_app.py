8f58fa1fc856891ad8ec3fdeb9e0ea06253c6470
***** <gsood07@gmail.com>
2023-04-06 15:40:06 -0700

    basic streamlit app

---
+++ streamlit/streamlit_app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import notnews
+
+# Set app title
+st.title("Not News")
+
+# Add a file uploader widget for the user to upload a CSV file
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+# Add a button to trigger the transformation
+if st.button("Transform Data"):
+    # Check if a file was uploaded
+    if uploaded_file is not None:
+        # Read the uploaded CSV file into a Pandas DataFrame
+        df = pd.read_csv(uploaded_file)
+        
+        # Use the package to transform the DataFrame
+        transformed_df = soft_news_url_cat_us(df, col='url')
+        
+        # Display the transformed DataFrame as a table
+        st.write(transformed_df)
+    else:
+        # Display an error message if no file was uploaded
+        st.error("Please upload a CSV file to transform.")
+
