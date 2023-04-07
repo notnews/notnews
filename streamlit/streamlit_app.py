56ad3cf4e512351f17d907916bf9cba960493c0e
***** <gsood07@gmail.com>
2023-04-06 17:35:10 -0700

    Update streamlit_app.py

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -17,7 +17,7 @@ if st.button("Transform Data"):
         df = pd.read_csv(uploaded_file)
         
         # Use the package to transform the DataFrame
-        transformed_df = soft_news_url_cat_us(df, col='url')
+        transformed_df = soft_news_url_cat(df, col='url')
         
         # Display the transformed DataFrame as a table
         st.write(transformed_df)
